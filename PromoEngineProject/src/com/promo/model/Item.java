61c0194588853249d8a2a5525eade47acff0c2bf
monishbkalmady <monishbkalmady@gmail.com>
2023-03-20 22:25:30 +0000

    Add: New java model class for items

---
+++ PromoEngineProject/src/com/promo/model/Item.java
@@ -0,0 +1,25 @@
+package com.promo.model;
+
+public class Item {
+	private String name;
+	private int quantity;
+	
+	public Item(String name, int quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
