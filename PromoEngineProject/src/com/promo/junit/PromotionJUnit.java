5f9ca900a09b35dc1d35d775a1c0f856731df382
monishbkalmady <monishbkalmady@gmail.com>
2023-03-20 21:41:12 +0000

    Add: New Junit test file

---
+++ PromoEngineProject/src/com/promo/junit/PromotionJUnit.java
@@ -0,0 +1,14 @@
+package com.promo.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PromotionJUnit {
+
+	@Test
+	void sampleTest() {
+		fail("Not yet implemented");
+	}
+
+}
